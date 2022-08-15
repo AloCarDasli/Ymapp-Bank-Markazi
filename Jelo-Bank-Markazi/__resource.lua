1760daa6b791bd1446dc09c6ee2e239721e5e67f
AloCarD <110132631+AloCarDasli@users.noreply.github.com>
2022-08-15 18:38:29 +0430

    Mapperi Bank Markazi

---
+++ Jelo-Bank-Markazi/__resource.lua
@@ -0,0 +1,35 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+this_is_a_map 'yes'
+
+
+files {
+
+    --country-trees
+    'stream/frosya__metadata/azozfrosya.ytyp',
+	'builderdef.ytyp'
+
+
+}
+
+--country-trees
+data_file 'DLC_ITYP_REQUEST' 'stream/frosya__metadata/azozfrosya.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/builderdefaddons.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'builderdef.ytyp'
+
+
+
+client_scripts {
+	'client.lua'
+}
+
+
+client_script "69.lua"
+
+
+--███████╗██████╗░  ███████╗██╗██╗░░░░░███████╗░██████╗
+--╚════██║██╔══██╗  ██╔════╝██║██║░░░░░██╔════╝██╔════╝
+--░░███╔═╝██████╔╝  █████╗░░██║██║░░░░░█████╗░░╚█████╗░
+--██╔══╝░░██╔══██╗  ██╔══╝░░██║██║░░░░░██╔══╝░░░╚═══██╗
+--███████╗██║░░██║  ██║░░░░░██║███████╗███████╗██████╔╝
+--╚══════╝╚═╝░░╚═╝  ╚═╝░░░░░╚═╝╚══════╝╚══════╝╚═════╝░
